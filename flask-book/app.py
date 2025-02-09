712d4c9f42abae8e9090dd0d4db8c8a86708b61d
Hants Williams <hantsawilliams@gmail.com>
2025-02-09 16:49:38 -0500

    added flask book

---
+++ flask-book/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+## chapter 1
+@app.route('/chapter1')
+def chapter1():
+    return render_template('chapter1/page1.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5002)
---
+++ flask-book/static/docs/chapter1_example1.md
@@ -0,0 +1,11 @@
+# Test Markdown
+
+## This is a test section 
+
+```python
+print("Hello World")
+```
+
+In this section, we will **test** the markdown parser.
+
+### Test 1
